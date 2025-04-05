9a1c6611502965d1897f48543e48c226757a6a83
BotSquirrel <60887692+BotSquirrel@users.noreply.github.com>
2025-04-05 18:10:42 +0200

    add dangerous permission

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,47 +1,27 @@
 package com.example.myapplication
 
-import android.Manifest
-import android.app.AlertDialog
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.example.myapplication.domain.repository.StratagemRepository
 import com.example.myapplication.domain.service.StratagemService
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import com.example.myapplication.utils.PermissionHelper
 
 class MainActivity : ComponentActivity() {
 
     private lateinit var stratagemRepository: StratagemRepository
-    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+    private lateinit var permissionHelper: PermissionHelper
+    private lateinit var requestPermissionsLauncher: ActivityResultLauncher<Array<String>>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        requestPermissionLauncher = registerForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) { isGranted: Boolean ->
-            if (isGranted) {
-                startStratagemService()
-            } else {
-                Toast.makeText(
-                    this,
-                    "Permission denied. Cannot load active stratagems.",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        }
-
-        maybeRequestNotificationPermission()
 
         stratagemRepository = StratagemRepository(this)
         setContent {
@@ -49,48 +29,65 @@ class MainActivity : ComponentActivity() {
                 Navigation(stratagemRepository = stratagemRepository)
             }
         }
+
+        askForPermissions()
+    }
+
+    private fun askForPermissions() {
+        permissionHelper = PermissionHelper(this)
+
+        requestPermissionsLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            permissionHelper.handlePermissionResults(permissions, {
+                startStratagemService()
+            }, { permission ->
+                when (permission) {
+                    android.Manifest.permission.POST_NOTIFICATIONS -> {
+                        Toast.makeText(
+                            this,
+                            "Notification permission denied. Cannot load active stratagems.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    android.Manifest.permission.ACCESS_FINE_LOCATION -> {
+                        Toast.makeText(
+                            this,
+                            "Location permission denied. Cannot share your location to regroup.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            })
+        }
+
+        permissionHelper.requestPermissions(requestPermissionsLauncher, arrayOf(
+            android.Manifest.permission.POST_NOTIFICATIONS,
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        ), {
+            startStratagemService()
+        }, { permission ->
+            if (permission == android.Manifest.permission.POST_NOTIFICATIONS) {
+                Toast.makeText(
+                    this,
+                    "Notification permission denied. Cannot load active stratagems.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            if (permission == android.Manifest.permission.ACCESS_FINE_LOCATION) {
+                Toast.makeText(
+                    this,
+                    "Location permission denied. Cannot share your location to regroup.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
     }
 
     private fun startStratagemService() {
         Intent(this@MainActivity, StratagemService::class.java).also {
             it.action = StratagemService.StratagemServiceAction.START.name
             startService(it)
-        }
-    }
-
-    private fun maybeRequestNotificationPermission() {
-        when {
-            ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.POST_NOTIFICATIONS
-            ) == PackageManager.PERMISSION_GRANTED -> {
-                startStratagemService()
-            }
-            ActivityCompat.shouldShowRequestPermissionRationale(
-                this,
-                Manifest.permission.POST_NOTIFICATIONS
-            ) -> {
-                AlertDialog.Builder(this)
-                    .setIcon(R.drawable.sos_beacon)
-                    .setTitle("Permission Required for active stratagems")
-                    .setMessage("In order to use this feature, we need permission to send you notifications in order to create a notification with the active stratagem.")
-                    .setPositiveButton("Allow") { _, _ ->
-                        requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
-                    }
-                    .setNegativeButton("Deny") { dialog, _ ->
-                        dialog.dismiss()
-                        Toast.makeText(
-                            this,
-                            "Permission is required for active stratagems.",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                    .setCancelable(false)
-                    .show()
-            }
-            else -> {
-                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
-            }
         }
     }
 }
